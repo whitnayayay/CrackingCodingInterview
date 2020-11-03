582834ed22fd7e460a24e778dbb48c615080a42d
whitnayayay <whitneythung@yahoo.com>
2020-11-03 13:29:14 -0800

    initial commit

---
+++ CrackingCodingInterview/src/ArraysAndStrings/isUniqueChars.java
@@ -0,0 +1,24 @@
+package ArraysAndStrings;
+
+//Implement an algorithm to determine if a string has all unique characters.
+public class isUniqueChars {
+
+	boolean isUniqueChars(String str) {
+		if (str.length() > 128)
+			return false;
+		
+		boolean[] char_set = new boolean[128]; 
+		for (int i = 0; i < str.length(); i++) 
+		{
+			int val = str.charAt(i);
+			if (char_set[val]) {
+				return false;
+			}
+			char_set[val] = true;
+		}
+		return true;
+	}
+	
+	
+	
+}
